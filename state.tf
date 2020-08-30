66ef95c3d6a7148ac7e54bce6e6d8365bdeef538
leorvieira <44712270+leorvieira@users.noreply.github.com>
2020-08-30 06:50:35 -0300

    Update state.tf
    hackathon-fiap-35scx-335196

--- state.tf
+++ state.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "hackathon-fiap-12345"
+    bucket = "hackathon-fiap-35scx-335196"
     key    = "state/hackaton-cicd-deploy"
     region = "us-east-1"
   }
